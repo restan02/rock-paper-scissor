5d4967e1d23db5003002e5e52a87ef2607bde163
tan02 <112875655+restan02@users.noreply.github.com>
2023-10-19 15:13:40 +0800

    Modified the playRoundfunc, added draw condition

--- script.js
+++ script.js
@@ -2,14 +2,18 @@ function getComputerChoice(){
     return Math.floor(Math.random() * 3)
 }
 
-
 function playRound(playerSelection, computerSelection){
 if(playerSelection === computerSelection+1||computerSelection === playerSelection-1){
     console.log(`Player: ${playerSelection}
-    Computer: ${computerSelection}
-    Player Wins!`)}
+                 Computer: ${computerSelection}
+                 Player Wins!`)}
+else if(playerSelection===computerSelection){
+    console.log(`Player: ${playerSelection}
+                 Computer: ${computerSelection}
+                 It's a draw!`)}
 else{
     console.log(`Player: ${playerSelection}
-    Computer: ${computerSelection}
-    Computer Wins!`)}
+                 Computer: ${computerSelection}
+                 computer Wins!`)}
+}
 }
