e7711d3c7bf64f0ce3acce07d7188501af8b88eb
tan02 <112875655+restan02@users.noreply.github.com>
2023-10-19 21:48:16 +0800

    updated the logic of win conditions

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let pScore = 0
 let cScore = 0
+const result = ["Player Wins", "Computer wins", "It's a Draw!"]
 const rcs = ["rock","paper","scissor"]
 
 function getComputerChoice(){
@@ -8,27 +9,29 @@ function getComputerChoice(){
 
 function playRound(playerSelection, computerSelection){
 if(playerSelection === computerSelection+1||playerSelection === computerSelection-2){
-    console.log(
-        `Player: ${rcs[playerSelection]}
-        Computer: ${rcs[computerSelection]}
-        Player Wins!`)}
+    console.log(`Player: ${rcs[playerSelection]}`)
+    console.log(`Computer: ${rcs[computerSelection]}`)
+    return result[0]
+    }
+        
 else if(playerSelection===computerSelection){
-    console.log(
-        `Player: ${rcs[playerSelection]}
-        Computer: ${rcs[computerSelection]}
-        It's a draw!`)}
+    console.log(`Player: ${rcs[playerSelection]}`)
+    console.log(`Computer: ${rcs[computerSelection]}`)
+    return result[2]
+    }
 else{
-    console.log(
-        `Player: ${rcs[playerSelection]}
-        Computer: ${rcs[computerSelection]}
-        computer wins Wins!`)}
+    console.log(`Player: ${rcs[playerSelection]}`)
+    console.log(`Computer: ${rcs[computerSelection]}`)
+    return result[1]
+    }
 }
 
 function game(){
 
     for(let i = 1; i<=5; i++){
     console.log(`Round ${i}`)
-
-
+    let roundResult = playRound(prompt("Please pick a number from 0-2"), getComputerChoice())
+    console.log(roundResult)
+   
     }
 }
