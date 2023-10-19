6563694ca11244235c7f5ee4bc2ba5c91830de3c
tan02 <112875655+restan02@users.noreply.github.com>
2023-10-19 15:09:33 +0800

    Added conditions in playRound func

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ function getComputerChoice(){
     return Math.floor(Math.random() * 3)
 }
 
-function playRound(playerSelection, computerSelection){
 
+function playRound(playerSelection, computerSelection){
+if(playerSelection === computerSelection+1||computerSelection === playerSelection-1){
+    console.log(`Player: ${playerSelection}
+    Computer: ${computerSelection}
+    Player Wins!`)}
+else{
+    console.log(`Player: ${playerSelection}
+    Computer: ${computerSelection}
+    Computer Wins!`)}
 }
