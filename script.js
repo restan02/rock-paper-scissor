5771e82b73b3241fa52656b96bb8f6609838f32e
tan02 <112875655+restan02@users.noreply.github.com>
2023-10-19 21:20:03 +0800

    Updated the logic for winning

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
+let pScore = 0
+let cScore = 0
+const rcs = ["rock","paper","scissor"]
+
 function getComputerChoice(){
     return Math.floor(Math.random() * 3)
 }
 
 function playRound(playerSelection, computerSelection){
-if(playerSelection === computerSelection+1||computerSelection === playerSelection-1){
-    console.log(`Player: ${playerSelection}
-                 Computer: ${computerSelection}
-                 Player Wins!`)}
+if(playerSelection === computerSelection+1||playerSelection === computerSelection-2){
+    console.log(
+        `Player: ${rcs[playerSelection]}
+        Computer: ${rcs[computerSelection]}
+        Player Wins!`)}
 else if(playerSelection===computerSelection){
-    console.log(`Player: ${playerSelection}
-                 Computer: ${computerSelection}
-                 It's a draw!`)}
+    console.log(
+        `Player: ${rcs[playerSelection]}
+        Computer: ${rcs[computerSelection]}
+        It's a draw!`)}
 else{
-    console.log(`Player: ${playerSelection}
-                 Computer: ${computerSelection}
-                 computer Wins!`)}
+    console.log(
+        `Player: ${rcs[playerSelection]}
+        Computer: ${rcs[computerSelection]}
+        computer wins Wins!`)}
 }
+
+function game(){
+
+    for(let i = 1; i<=5; i++){
+    console.log(`Round ${i}`)
+
+
+    }
 }
